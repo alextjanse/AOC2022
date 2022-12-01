a8751f2aa5b1918281063d877e9516fc3bdc40cb
AlexJohnso <github@alexjanse.nl>
2022-12-01 17:23:42 +0100

    Day 1

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+    public class Exercise {
+        public string[] input;
+
+        public Exercise(string name, string[] input) {
+            Console.WriteLine($"Day {name}");
+            this.input = input;
+        }
+
+        public virtual void solvePart1() {
+            Console.Write("Part One: ");
+        }
+
+        public virtual void solvePart2() {
+            Console.Write("Part Two: ");
+        }
+    }
+
+    internal class Program {
+        static void Main(string[] args) {
+            foreach (string s in args) {
+                string[] input = readInput(s);
+                Exercise e;
+
+                switch (s) {
+                    case "1":
+                        e = new Exercise1(s, input);
+                        break;
+                    default:
+                        throw new Exception("invalid argument");
+                }
+
+                e.solvePart1();
+                e.solvePart2();
+
+                Console.WriteLine(); // empty line seperating the exercise outputs
+            }
+        }
+
+        static string[] readInput(string exercise) {
+            StreamReader sr = new StreamReader($"./input/{exercise}.txt");
+
+            return sr.ReadToEnd().Split('\n');
+        }
+    }
+}
---
+++ exercises/1.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace AdventOfCode {
+    public class Exercise1 : Exercise {
+        public Exercise1(string name, string[] input) : base(name, input) {
+            
+        }
+
+        public override void solvePart1()
+        {
+            base.solvePart1();
+
+            int max = 0;
+
+            int current = 0;
+
+            foreach (string line in input) {
+                if (line == "") {
+                    if (current > max) {
+                        max = current;
+                    }
+
+                    current = 0;
+                    continue;
+                }
+
+                current += int.Parse(line);
+            }
+
+            if (current > max) {
+                max = current;
+            }
+
+            Console.WriteLine(max);
+        }
+
+        public override void solvePart2()
+        {
+            base.solvePart2();
+
+            int[] top3 = new int[] {0, 0, 0};
+
+            int current = 0;
+
+            foreach (string line in input) {
+                if (line == "") {
+                    replaceTop3(top3, current);
+
+                    current = 0;
+                    continue;
+                }
+
+                current += int.Parse(line);
+            }
+
+            replaceTop3(top3, current);
+
+            Console.WriteLine(top3.Sum());
+        }
+
+        void replaceTop3(int[] top3, int value) {
+            for (int i = 0; i < 3; i++) {
+                if (value > top3[i]) {
+                    int temp = top3[i];
+                    top3[i] = value;
+                    value = temp;
+                }
+            }
+        }
+    }
+}
